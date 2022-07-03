5503f3a65c8f13654441f779d4dfb4dfcef74942
glitchy <thepatheticweebgamer@gmail.com>
2022-07-03 16:47:09 -0400

    :sparkles: feat: Added theme to daisyui to make theming easier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/*.jinja2"],
-  theme: {},
+  daisyui: {
+      themes: [
+        {
+          main: {
+            "primary": '#761D3A',
+            "secondary": "#D926A9",
+            "accent": "#1FB2A6",
+            "neutral": "#191D24",
+            "base-100": "#2A303C",
+            "info": "#3ABFF8",
+            "success": "#36D399",
+            "warning": "#FBBD23",
+            "error": "#f43f5e",
+          },
+        },
+      ],
+  },
   plugins: [require('daisyui')],
   theme: {
     colors: {
       'midnight': {
-        100: '#1e1928',
-        200: '#231e2d',
+        100: '#1E1928',
+        200: '#231E2d',
+        300: '#1a1621',
       },
       'baelz': {
         100: '#dd2a62',
