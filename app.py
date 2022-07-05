2a5c614aee423da55f507a6723940ef0018ea40a
glitchy <thepatheticweebgamer@gmail.com>
2022-07-04 23:31:19 -0400

    :recycle: refactor(projects): Updated `projects.jinja2` making it look better with smaller code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from dotenv import load_dotenv
 from github import Github
@@ -6,6 +7,21 @@ from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
+
+
+def get_readme_image(repo):
+    try:
+        readme = repo.get_contents("README.md")
+    except:
+        return None
+
+    images = re.findall(r'src="([^"]+)"', str(readme.decoded_content))
+    if len(images) >= 1:
+        img_url = repo.get_contents(images[0]).download_url
+    else:
+        return None
+
+    return img_url
 
 
 @app.route('/')
@@ -21,7 +37,7 @@ async def project_page():
     for repo in user.get_repos():
         if not repo.fork and not repo.name.lower() == "glitchchan":
             repos.append(repo)
-    return render_template('projects.jinja2', repos=repos)
+    return render_template('projects.jinja2', repos=repos, get_readme_image=get_readme_image)
 
 
 if __name__ == '__main__':
