467eb849ae7d0fd37fa963c87d9dc337cb3b791a
glitchy <thepatheticweebgamer@gmail.com>
2022-07-07 15:05:55 -0400

    :truck: moved: Moved from using Flask to Quart

--- app.py
+++ app.py
@@ -1,44 +1,24 @@
-import os
-import re
+from src import get_readme_image, get_repos
+from quart import Quart, render_template
 
-from dotenv import load_dotenv
-from github import Github
-from flask import Flask, render_template
-
-load_dotenv()
-app = Flask(__name__)
-
-
-def get_readme_image(repo):
-    try:
-        readme = repo.get_contents("README.md")
-    except:
-        return None
-
-    images = re.findall(r'src="([^"]+)"', str(readme.decoded_content))
-    if len(images) >= 1:
-        img_url = repo.get_contents(images[0]).download_url
-    else:
-        return None
-
-    return img_url
+app = Quart(__name__)
 
 
 @app.route('/')
 async def index():
-    return render_template('index.jinja2')
+    return await render_template('index.jinja2')
+
+
+@app.route('/about')
+async def about_page():
+    return await render_template('about.jinja2')
 
 
 @app.route('/projects')
 async def project_page():
-    git = Github(os.environ.get('GITHUB'))
-    user = git.get_user('glitchchan')
-    repos = []
-    for repo in user.get_repos():
-        if not repo.fork and not repo.name.lower() == "glitchchan":
-            repos.append(repo)
-    return render_template('projects.jinja2', repos=repos, get_readme_image=get_readme_image)
+    repos = get_repos()
+    return await render_template('projects.jinja2', repos=repos, get_readme_image=get_readme_image)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
